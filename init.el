4fce38d1182ae4aea600c81240388157a5d638e5
Putranto <p.hadiutomo@gmail.com>
2015-03-19 08:32:38 +0100

    Create a

---
+++ init.el
@@ -0,0 +1,37 @@
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  )
+
+(global-linum-mode 1)
+
+(require 'auto-complete-sage)
+
+(global-auto-complete-mode t)
+
+(require 'auto-complete-auctex)
+
+
+
+(add-hook 'lisp-mode-hook '(lambda ()  (local-set-key (kbd "RET") 'newline-and-indent))) ;;not working
+
+
+(show-paren-mode t)               ;; turn paren-mode on
+(set-face-foreground 'show-paren-mismatch-face "red") 
+(set-face-attribute 'show-paren-mismatch-face nil 
+                    :weight 'bold :underline t :overline nil :slant 'normal)
+
+;;;latex normal font
+;; Only change sectioning colour
+(setq font-latex-fontify-sectioning 'color)
+;; super-/sub-script on baseline
+(setq font-latex-script-display (quote (nil)))
+;; Do not change super-/sub-script font
+(custom-set-faces
+ '(font-latex-subscript-face ((t nil)))
+ '(font-latex-superscript-face ((t nil)))
+ )
+;; Exclude bold/italic from keywords
+(setq font-latex-deactivated-keyword-classes
+    '("italic-command" "bold-command" "italic-declaration" "bold-declaration"))
