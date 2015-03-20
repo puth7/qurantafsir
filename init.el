a7a853c9f95cd8897498f9acbf7ca109e9cd20ef
Putranto <p.hadiutomo@gmail.com>
2015-03-20 06:02:39 +0100

    auto indent update, registry to init file,

--- init.el
+++ init.el
@@ -1,31 +1,44 @@
+
+
+;(define-key global-map (kbd "<apps>") 'execute-extended-command);; untuk winsdows (komoputer kantor
+;----------------------------------------
+
+
+
+
 (when (>= emacs-major-version 24)
   (require 'package)
   (package-initialize)
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
   )
 
+
+
+;;(define-key global-map (kbd "RET") 'newline-and-indent)
+;;(define-key global-map (kbd "<apps>") 'execute-extended-command)
+
 (global-linum-mode 1)
 
+;autocoomplete--------------------
 (require 'auto-complete-sage)
-
 (global-auto-complete-mode t)
-
 (require 'auto-complete-auctex)
-;;test
-					;tes
-;coba lt
-
-(define-key global-map (kbd "RET") 'newline-and-indent)
-(define-key global-map (kbd "<apps>") 'execute-extended-command)
+;----------------------------------------
 
 
 
+
+
+
+;;; parentheses marking ---------------------
 (show-paren-mode t)               ;; turn paren-mode on
 (set-face-foreground 'show-paren-mismatch-face "red") 
 (set-face-attribute 'show-paren-mismatch-face nil 
                     :weight 'bold :underline t :overline nil :slant 'normal)
+;------------------------------------------
 
-;;;latex normal font
+
+;;;latex normal font-------------------------
 ;; Only change sectioning colour
 (setq font-latex-fontify-sectioning 'color)
 ;; super-/sub-script on baseline
@@ -35,17 +48,41 @@
  '(font-latex-subscript-face ((t nil)))
  '(font-latex-superscript-face ((t nil)))
  )
-;; Exclude bold/italic from keywords
+; Exclude bold/italic from keywords
 (setq font-latex-deactivated-keyword-classes
     '("italic-command" "bold-command" "italic-declaration" "bold-declaration"))
+
 ;;  (eval-after-load "tex-mode" '(fset 'tex-font-lock-suscript 'ignore)) ;; disable auto subscript for latex file
 
 
 ;(require 'bibtex)
 
+;-------------------------------------------
+;;(cd "/home/puth/Documents/Putranto/TUe/Binary sudoku") not working
+
+
 (setq reftex-plug-into-AUCTex t)
 (setq-default TeX-PDF-mode t)
 (setq-default TeX-engine 'luatex)
+
 ;;(setq latex-run-command "pdflatex")
 
-(cd "E:\\s3\\coding_theory\\modulation_codes-erasure_decoding\\paper")
+;;(cd "E:\\s3\\coding_theory\\modulation_codes-erasure_decoding\\paper")
+
+
+(set-register ?i (cons 'file "~/.emacs.d/init.el"))
+
+
+;enter auto indent---------------------
+
+  (setq auto-indent-on-visit-file t) ;; If you want auto-indent on for files
+  (require 'auto-indent-mode)
+  (auto-indent-global-mode)
+
+
+
+;;(electric-indent-mode 1)
+;(define-key global-map (kbd "RET") 'newline-and-indent)
+
+;;(add-hook 'lisp-mode-hook '(lambda ()  (local-set-key (kbd "RET") 'newline-and-indent))) ;;not working
+
